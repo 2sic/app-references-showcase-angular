56f0cfa329af84baa1e6a779660becd02ffdd427
Benetu Gschwend <benetu.gschwend@2sic.com>
2019-06-27 09:35:27 +0200

    Delete spec.ts files

--- src/app/core/services/sxc-data/sxc-data.service.ts
+++ src/app/core/services/sxc-data/sxc-data.service.ts
@@ -1,9 +1,9 @@
+import { Data } from '@2sic.com/dnn-sxc-angular';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 import { Category } from '../../../shared/interfaces/category.interfaces';
 import { Reference } from '../../../shared/interfaces/references.interfaces';
-import { Data } from '@2sic.com/dnn-sxc-angular';
-import { map, shareReplay } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class SxcDataService {
@@ -14,7 +14,7 @@ export class SxcDataService {
     constructor(
         private data: Data
     ) {
-        this.references$ = this.data.content<Reference>('Reference').get().pipe(shareReplay());
-        this.categories$ = this.data.content<Category>('Category').get().pipe(shareReplay());
+      this.references$ = this.data.content<Reference>('Reference').get().pipe(shareReplay());
+      this.categories$ = this.data.content<Category>('Category').get().pipe(shareReplay());
     }
 }
