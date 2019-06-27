35f0b6aec2bd6ac3b79fef5f789f591cc34644d7
Benetu Gschwend <benetu.gschwend@2sic.com>
2019-06-27 10:30:59 +0200

    filter references by category

--- src/app/core/services/catergory-filter/category-filter.service.ts
+++ src/app/core/services/catergory-filter/category-filter.service.ts
@@ -1,20 +1,49 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { combineLatest, Observable, Subject } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 import { Category } from 'src/app/shared/interfaces/category.interfaces';
+import { Reference } from 'src/app/shared/interfaces/references.interfaces';
+import { SxcDataService } from '../sxc-data/sxc-data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryFilterService {
 
+  referencesBySelectedCategory$: Observable<Reference[]>;
+
   selectedCategory$: Observable<Category>;
   private selectedCategorySubjet$ = new Subject<Category>();
 
-  constructor() {
-    this.selectedCategory$ = this.selectedCategorySubjet$;
+  constructor(
+    sxcData: SxcDataService,
+  ) {
+
+    this.selectedCategory$ = this.selectedCategorySubjet$.pipe(shareReplay());
+
+    this.referencesBySelectedCategory$ = combineLatest(
+      this.selectedCategory$,
+      sxcData.references$,
+    ).pipe(
+      map(([category, references]) =>
+        // if Id is null return all references
+        !!category.Id
+        ? referencesByCategory(category, references)
+        : references
+      ),
+      shareReplay(),
+    );
   }
 
   updateSelectedCategory(category: Category) {
     this.selectedCategorySubjet$.next(category);
   }
+}
+
+function referencesByCategory(category: Category, references: Reference[]): Reference[] {
+  const filteredRefs: Reference[] = references.filter(ref => {
+    return ref.Category.some(cat => cat.Id === category.Id);
+  });
+
+  return filteredRefs;
 }
